bcd5309d55ae4f42939d49fd88987d781186eeb0
devloperKamble <komaltanuja2714@gmail.com>
2022-01-25 11:01:09 +0530

    calc

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,32 @@
 
 
 class Calculator{
+      
+    static void add(int a, int b){
+        int c=a+b;
+        System.out.println("Addition =>"+c);
+    }
+    static void sub(int a, int b){
+        int c=b-a;
+        System.out.println("Substraction =>"+c);
+    }
+    static void div(int a, int b){
+        int c=a*b;
+        System.out.println("Multiplication =>"+c);
+    }
+    static void mult(int a, int b){
+        float c=b/a;
+        System.out.println("Division =>"+c);
+    }
+
     public static void main(String[] args){
         int a=10,b=20;
         //abc
+        add(a,b);
+        sub(a,b);
+        div(a,b);
+        mult(a,b);
+
+    
     }
 }
